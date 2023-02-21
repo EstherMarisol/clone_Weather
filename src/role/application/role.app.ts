39f93cb3325017ceda611126f69c3d3a546c6061
kcor <102560068+niloDagmar@users.noreply.github.com>
2023-02-20 22:32:58 -0400

    update role get by code

--- src/role/application/role.app.ts
+++ src/role/application/role.app.ts
@@ -1,4 +1,4 @@
-import { saveRoleService } from "../domain/role.services";
+import { getRoleService, saveRoleService } from "../domain/role.services";
 
 export const saveRoleApp = async (reqUser: any, data: any) => {
   const roleToSave = {
@@ -7,4 +7,9 @@ export const saveRoleApp = async (reqUser: any, data: any) => {
   const roleSaved = await saveRoleService(roleToSave);
   return roleSaved;
 
+}
+export const getRoleByCodeApp = async (reqUser: any, code: string) => {
+  const codeNow = parseInt(code);
+  const role = await getRoleService(codeNow);
+  return role;
 }
